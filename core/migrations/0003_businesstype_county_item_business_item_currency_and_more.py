b7120cedfda65d5a621e7e81c8710c7b2bad36e2
mwendwaroy-Angoo <mwendwaroy@gmail.com>
2026-03-23 00:52:52 +0300

    modified business null model

--- core/migrations/0003_businesstype_county_item_business_item_currency_and_more.py
+++ core/migrations/0003_businesstype_county_item_business_item_currency_and_more.py
@@ -51,7 +51,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='store',
             name='business',
-            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, related_name='stores', to='accounts.business'),
+            field=models.ForeignKey(null=True, blank=True, on_delete=django.db.models.deletion.CASCADE, related_name='stores', to='accounts.business'),      #temporarily allow null
             preserve_default=False,
         ),
         migrations.AddField(
