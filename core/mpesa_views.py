6f190ad70276536f439c419d5c54e7a865318557
mwendwaroy-Angoo <mwendwaroy@gmail.com>
2026-04-08 21:41:24 +0300

    Fix: import render/redirect in mpesa_views for pending_prompts page

--- core/mpesa_views.py
+++ core/mpesa_views.py
@@ -11,6 +11,7 @@ import json
 import logging
 
 from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST, require_GET
